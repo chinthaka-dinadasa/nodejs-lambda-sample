8490a19c4b38b05342db7014b31ca5368381ce25
Chinthaka Dinadasa <sendtoclabz@gmail.com>
2024-12-26 21:49:22 +0530

    handler and serverless added

--- handler.js
+++ handler.js
@@ -1,8 +1,11 @@
+const { v4: uuidv4 } = require("uuid");
+
 exports.hello = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: "Go Serverless v4! Your function executed successfully!",
+      sample_data_id: uuidv4(),
     }),
   };
 };
